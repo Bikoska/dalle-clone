0caffaf91ce1d7e8194a656b9ebf5abd4c0abf2a
Bikoska <kristinabikoska@gmail.com>
2023-02-06 14:58:58 +0100

    before backend

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -32,7 +32,9 @@ const FormField = ({
       type={type}
       id={name}
       name={name}
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+      className="bg-gray-50 border border-gray-300 
+      text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] 
+      focus:border-[#6469ff] outline-none block w-full p-3"
       placeholder={placeholder}
       value={value}
       onChange={handleChange}
